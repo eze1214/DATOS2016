709c859432d032011fdb07d503d624eedda444da
Eze <eze1214@live.com.ar>
2016-09-19 01:36:03 -0300

    Faltaban archvos

--- tp/archrrlv.cpp
+++ tp/archrrlv.cpp
@@ -28,23 +28,23 @@ bool ArchRRLV::existeArchivo(string nombreArchivo){
 void ArchRRLV::crearArchivo(std::string nombreArchivo,std::string configuracion){
 	FILE * archivo;
 	archivo = fopen(nombreArchivo.data(),configuracion.c_str());
-	if (archivo != NULL) std::cout<<"archivo creado correctamente"<<endl;
+	if (archivo != NULL) std::cout<<"archivo creado correctamente"<<std::endl;
 	fclose (archivo);
 
 }
 
 void ArchRRLV::addRegistro(Registro & registro){
 	std::string serializado;
-	registro.serializar(serializado);
-	
-	
+	registro.serializar(serializado);	
 }
 
 //Sobrescribe el numero de bloque especificado
 void ArchRRLV::grabarRegistro(const std::string & registro){
 	FILE *archivo;
 	archivo = fopen (filename.data(),"r+b");
-	fseek(archivo,0,SEEK_END);//Calcularla
+	fseek(archivo,0,SEEK_);//Calcularla
+	int size = registro.size();
+	fwrite((char*)&size,1,sizeof(int),archivo);
 	fwrite (registro.c_str(),1,registro.size(),archivo);
 	fclose(archivo);
 }
@@ -52,6 +52,21 @@ void ArchRRLV::grabarRegistro(const std::string & registro){
 
 Registro ArchRRLV::readRegistro(){
 		//TODO Poner la logica para no leer el bloque de espacios libre
-	
+	FILE *archivo;
+	archivo = fopen (filename.data(),"rb");	
+	fseek(archivo,puntero,SEEK_CUR);
+	int size=0;
+	puntero += fread((char*)&size,sizeof(int),1,archivo);
+	char * buffer = new char[size];
+	//TODO falta controlar el fin de archivo;
+	puntero += fread((char*)buffer,size,1,archivo);
+	Registro registro(this->formato);
+	registro.serializar(buffer,size);
+	delete buffer[];
+	fclose(archivo);
+	return registro;
+}
+
+Registro ArchRRLV::delRegistro(){
 		
 }
