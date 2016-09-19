b8d0c15414ba11b1bf8bf15334b6393e9573e2cd
ezequiel dufau <eze1214@live.com.ar>
2016-09-19 11:51:59 -0300

    Terminar RRLV

--- tp/archrrlv.cpp
+++ tp/archrrlv.cpp
@@ -1,6 +1,6 @@
 #include "archrrlv.h"
 #include "registro.h"
-
+#include <cstdio>
 
 ArchRRLV::ArchRRLV(std::string filename, std::string formato){
 	this->filename = filename;
@@ -12,11 +12,29 @@ ArchRRLV::ArchRRLV(std::string filename, std::string formato){
 	crearArchivo(filename,"wb+");
 }
 
-
+ArchRRLV::ArchRRLV(std::string filename){
+	this->filename = filename;
+	this->puntero = 0;
+	
+	FILE * archivo;
+	archivo = fopen(filename.data(),"rb");
+	formato.clear();
+	char leido=0;
+	do{
+		fread(&leido,sizeof(char),1,archivo);
+		formato+=leido;
+	}while(leido!='\n');
+	
+	char * buffer = new char [descriptor.descriptorSize()];
+		fread(buffer,descriptor.descriptorSize,1,archivo);
+		descriptor.hidratar(buffer,descriptor.descriptorSize());
+	delete [] buffer;
+	
+}
 bool ArchRRLV::existeArchivo(string nombreArchivo){
 	bool existencia;
 	FILE *archivo;
-	archivo =fopen(nombreArchivo.data(),"r+b");
+	archivo = fopen(nombreArchivo.data(),"r+b");
 	if (archivo != NULL) {
 		existencia = true;
 		fclose(archivo);
@@ -27,22 +45,43 @@ bool ArchRRLV::existeArchivo(string nombreArchivo){
 
 void ArchRRLV::crearArchivo(std::string nombreArchivo,std::string configuracion){
 	FILE * archivo;
+	char fin = '\n';
 	archivo = fopen(nombreArchivo.data(),configuracion.c_str());
 	if (archivo != NULL) std::cout<<"archivo creado correctamente"<<std::endl;
+	fwrite((char*)&formato.c_str(),formato.size(),1,archivo);
+	fwrite(&fin,sizeof(char),1,archivo);
+	bool esRegistro = false;
+	fwrite((char*)esRegistro,sizeof(bool),1,archivo);
+	unsigned size = descriptor.descriptorSize();
+	char * buffer = new char[size];
+	
+	descriptor.proximo = formato.size() + size + sizeof(char) + sizeof(bool);
+	descriptor.RegSize = 0;
+	descriptor.serializar(buffer,size);
+	
+	fwrite((char *)buffer,size,1,archivo);
+	delete [] buffer;
 	fclose (archivo);
-
 }
 
 void ArchRRLV::addRegistro(Registro & registro){
 	std::string serializado;
 	registro.serializar(serializado);	
+	grabarRegistro(serializado);
 }
 
-//Sobrescribe el numero de bloque especificado
+void ArchRRLV::readDescriptor(){
+	
+	fseek(archivo,formato.size+1);
+	
+}
+void ArchRRLV::buscarEspacioLibre(FILE * archivo){
+	readDescriptor();	
+}
 void ArchRRLV::grabarRegistro(const std::string & registro){
 	FILE *archivo;
 	archivo = fopen (filename.data(),"r+b");
-	fseek(archivo,0,SEEK_);//Calcularla
+	buscarEspacioLibre(archivo);
 	int size = registro.size();
 	fwrite((char*)&size,1,sizeof(int),archivo);
 	fwrite (registro.c_str(),1,registro.size(),archivo);
@@ -65,8 +104,4 @@ Registro ArchRRLV::readRegistro(){
 	delete buffer[];
 	fclose(archivo);
 	return registro;
-}
-
-Registro ArchRRLV::delRegistro(){
-		
 }
--- tp/archrrlv.h
+++ tp/archrrlv.h
@@ -3,8 +3,33 @@
 #include <string>
 #include <iostream>
 #include "registro.h"
+#include <cstring>
+
+ class DescriptorEspacioLibre{
+	friend class ArchRRLV;
+	unsigned RegSize;
+	unsigned proximo;
+	bool ultimo;	
+public:
+	DescriptorEspacioLibre():ultimo(true){}
+	void hidratar(char * buffer, unsigned size){
+		memcpy((char*)&size, buffer, sizeof(unsigned));
+		memcpy((char*)&proximo,buffer+sizeof(unsigned),sizeof(unsigned));
+		memcpy((char*)ultimo,buffer+sizeof(unsigned)+sizeof(unsigned),sizeof(bool));
+	}
+	void serializar(char * buffer, unsigned size){
+		memcpy(buffer,(char*)&size,sizeof(unsigned);
+		memcpy(buffer+sizeof(unsigned),(char*)&proximo,sizeof(unsigned));
+		memcpy(buffer+sizeof(unsigned)+sizeof(unsigned),(char*)&ultimo,sizeof(bool));
+	}
+	unsigned descriptorSize(){
+			return (sizeof(unsigned) + sizeof(unsigned) +sizeof(bool));
+	}
+	
+};
 class ArchRRLV{
 private:
+	DescriptorEspacioLibre descriptor;
 	std::string filename;
 	std::string formato;
 	void existeArchivo(string nombreArchivo);
