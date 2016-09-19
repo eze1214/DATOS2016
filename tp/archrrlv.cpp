e64668eeddeba8406421a7054fd5fbb9f7059467
Eze <eze1214@live.com.ar>
2016-09-19 01:16:19 -0300

    Creado el registro de longitud fija terminar

--- tp/archrrlv.cpp
+++ tp/archrrlv.cpp
@@ -1,18 +1,57 @@
-/*
- * Copyright 2016 <copyright holder> <email>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
-
 #include "archrrlv.h"
+#include "registro.h"
+
+
+ArchRRLV::ArchRRLV(std::string filename, std::string formato){
+	this->filename = filename;
+	this->formato = formato;
+	this->puntero = 0;
+	if (existeArchivo(filename)){
+		std::cout<<"El archivo ya existe será sobreescribido"<<std::endl;
+	}
+	crearArchivo(filename,"wb+");
+}
+
+
+bool ArchRRLV::existeArchivo(string nombreArchivo){
+	bool existencia;
+	FILE *archivo;
+	archivo =fopen(nombreArchivo.data(),"r+b");
+	if (archivo != NULL) {
+		existencia = true;
+		fclose(archivo);
+	}
+	else existencia = false;
+	return existencia;
+}
+
+void ArchRRLV::crearArchivo(std::string nombreArchivo,std::string configuracion){
+	FILE * archivo;
+	archivo = fopen(nombreArchivo.data(),configuracion.c_str());
+	if (archivo != NULL) std::cout<<"archivo creado correctamente"<<endl;
+	fclose (archivo);
+
+}
+
+void ArchRRLV::addRegistro(Registro & registro){
+	std::string serializado;
+	registro.serializar(serializado);
+	
+	
+}
+
+//Sobrescribe el numero de bloque especificado
+void ArchRRLV::grabarRegistro(const std::string & registro){
+	FILE *archivo;
+	archivo = fopen (filename.data(),"r+b");
+	fseek(archivo,0,SEEK_END);//Calcularla
+	fwrite (registro.c_str(),1,registro.size(),archivo);
+	fclose(archivo);
+}
+
+
+Registro ArchRRLV::readRegistro(){
+		//TODO Poner la logica para no leer el bloque de espacios libre
+	
+		
+}
--- tp/archrrlv.h
+++ tp/archrrlv.h
@@ -1,26 +1,22 @@
-/*
- * Copyright 2016 <copyright holder> <email>
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
-
 #ifndef ARCHRRLV_H
 #define ARCHRRLV_H
 #include <string>
+#include <iostream>
+#include "registro.h"
 class ArchRRLV{
-	ArchRRLV(std::string filename);
+private:
+	std::string filename;
+	std::string formato;
+	void existeArchivo(string nombreArchivo);
+	unsigned puntero;
 	
+public:
+	ArchRRLV(std::string filename,std::string formato);
+	crearArchivo(std::string filename, std::string configuracion);
+	
+	/*Lee un registro de forma secuencial
+	 */
+	Registro readRegistro();
 };
 
 #endif // ARCHRRLV_H
